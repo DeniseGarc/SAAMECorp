50b223f7ab23bc7932d8a93c87146d351efdad73
DeniseGarc <aliciadenise0623@gmail.com>
2025-03-18 09:22:45 -0700

    Commit inicial: creación del proyecto

---
+++ negocios_AgendarCita/src/main/java/com/negocios_agendarcita/Negocios_AgendarCita.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.negocios_agendarcita;
+
+/**
+ *
+ * @author Alici
+ */
+public class Negocios_AgendarCita {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
