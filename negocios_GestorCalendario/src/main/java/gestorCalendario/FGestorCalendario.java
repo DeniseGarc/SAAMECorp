c44ce4ef74fd98f4c51d4a7e8c5e82477a01a666
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-03-19 15:00:32 -0700

    fachada gestor prueba del push

--- negocios_GestorCalendario/src/main/java/gestorCalendario/FGestorCalendario.java
+++ negocios_GestorCalendario/src/main/java/gestorCalendario/FGestorCalendario.java
@@ -4,10 +4,27 @@
  */
 package gestorCalendario;
 
+import dto.PsicologoDTO;
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author Alici
  */
 public class FGestorCalendario implements IGestorCalendario{
+
+    @Override
+    public List<Date> diasAgendaLlena() {
+        
+    }
+
+    @Override
+    public List<Date> diasConReservas() {
+    }
+
+    @Override
+    public boolean diaDisponiblePsicologo(PsicologoDTO psicologo, Date fecha) {
     
+    }
 }
