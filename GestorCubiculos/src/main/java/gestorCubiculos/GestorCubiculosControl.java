8402980139622333dd156b0d368b1eee3d8c1dba
erika <erika.vasquez253264@potros.itson.edu.mx>
2025-05-16 19:35:51 -0700

    subsistema cubiculos

--- GestorCubiculos/src/main/java/gestorCubiculos/GestorCubiculosControl.java
+++ GestorCubiculos/src/main/java/gestorCubiculos/GestorCubiculosControl.java
@@ -4,12 +4,66 @@
  */
 package gestorCubiculos;
 
+import dto.CubiculoDTO;
+import static enumeradores.TipoBO.CUBICULO;
+import excepciones.GestorCubiculosException;
+import excepciones.NegocioException;
+import interfaces.ICubiculoBO;
+import java.util.List;
+import manejadorBO.ManejadorBO;
+
 /**
  *Clase que define los metodos del subsistema de Gestion de Cubiculos
  * 
  * @author erika
  */
 public class GestorCubiculosControl {
-
+    
+    ICubiculoBO cubiculoBO = (ICubiculoBO) ManejadorBO.crearBO(CUBICULO);
+    
+    public List<CubiculoDTO> ObtenerCubiculosPorEstado(boolean estado) throws GestorCubiculosException{
+        try{
+           return cubiculoBO.obtenerCubiculosPorEstado(estado);
+        }catch(NegocioException e ){
+            throw new GestorCubiculosException("Eror al obtener los cubiculos: "+e.getMessage());
+            
+        }
+    }
+    
+    public List<CubiculoDTO> obtenerCubiculos()throws GestorCubiculosException{
+        try{
+           return cubiculoBO.obtenerCubiculos();
+        }catch(NegocioException e ){
+            throw new GestorCubiculosException("Eror al obtener los cubiculos: "+e.getMessage());
+            
+        }
+    }
+    
+    public boolean AgregaeCubiculo(CubiculoDTO cubiculo) throws GestorCubiculosException{
+        try{
+            return cubiculoBO.agregarCubiculo(cubiculo);
+        }catch(NegocioException e ){
+            throw new GestorCubiculosException("Eror al agregar:"+e.getMessage());
+            
+        }
+    }
+    
+    public boolean ModificarCubiculo(CubiculoDTO cubiculoModificar ) throws GestorCubiculosException{
+        try{
+            return cubiculoBO.modificarCubiculo(cubiculoModificar);
+        }catch(NegocioException e ){
+            throw new GestorCubiculosException("Eror al agregar:"+e.getMessage());
+            
+        }
+    }
+    
+    public boolean ModificarEstado(CubiculoDTO cubiculo)throws GestorCubiculosException{
+        try{
+            return cubiculoBO.actualizarEstadoCubiculo(cubiculo);
+        }catch(NegocioException e ){
+            throw new GestorCubiculosException("Eror al agregar:"+e.getMessage());
+            
+        }
+    }
     
 }
