8bced42617e0e6dbe192327b1ebd5a7cc1e87053
DeniseGarc <aliciadenise0623@gmail.com>
2025-05-19 15:40:02 -0700

    PARCHE TEMPORAL DE METODOS QUE FALLABAN
    por el momento devuelven null o un valor defecto / estos cambios deben de eliminarse despues.

--- objetos_negocio/src/main/java/BO/ReporteCubiculoBO.java
+++ objetos_negocio/src/main/java/BO/ReporteCubiculoBO.java
@@ -28,28 +28,29 @@ import java.util.logging.Logger;
  *
  * @author erika
  */
-public class ReporteCubiculoBO implements IReporteCubiculoBO{
-    
+public class ReporteCubiculoBO implements IReporteCubiculoBO {
+
     /**
      * Instancia de la clase CitaDAO
      */
     ICitaDAO citaDAO = CitaDAO.getInstancia();
-    
+
     /**
      * Instancia unica de la clase
      */
     private static ReporteCubiculoBO instancia;
-    
+
     /**
      * Instancia de la clase CubiculoDAO
      */
     ICubiculoDAO cubiculoDAO = CubiculoDAO.getInstanciaDAO();
-    
+
     /**
      * Constructor privado para evitar instancias externas
      */
-    private ReporteCubiculoBO(){}
-    
+    private ReporteCubiculoBO() {
+    }
+
     /**
      * Metodo para obtener la instancia unica de CitaBO Si no existe una la crea
      *
@@ -61,116 +62,125 @@ public class ReporteCubiculoBO implements IReporteCubiculoBO{
         }
         return instancia;
     }
-    
+
     /**
-   * Metodo para ganerar un reporte de uso de cubiculos 
-   * @param nombreCubiculo Nombre del cubiculo al que se le va a generar el reportes
-   * @return Reporte de uso de cubiculo
-   * @throws NegocioException 
-   */
+     * Metodo para ganerar un reporte de uso de cubiculos
+     *
+     * @param nombreCubiculo Nombre del cubiculo al que se le va a generar el
+     * reportes
+     * @return Reporte de uso de cubiculo
+     * @throws NegocioException
+     */
     @Override
     public ReporteUsoCubiculoDTO generarReporteUsoCubiculo(String nombreCubiculo) throws NegocioException {
         try {
             List<Cita> citas = citaDAO.obtenerCitas();
             int usos = 0;
-            
+
             for (Cita cita : citas) {
-                if (nombreCubiculo.equalsIgnoreCase(cita.getCubiculo())) {
-                    usos++;
-                }
+//                if (nombreCubiculo.equalsIgnoreCase(cita.getCubiculo())) {
+//                    usos++;
+//                }
             }
-            
+
             return new ReporteUsoCubiculoDTO(nombreCubiculo, usos);
         } catch (PersistenciaException ex) {
             Logger.getLogger(ReporteCubiculoBO.class.getName()).log(Level.SEVERE, null, ex);
             throw new NegocioException("Error al generar el reporte", ex);
         }
     }
-    
+
     /**
-   * Metodo que genera un reporte estadistico sobre los cubiculos registrados
-   * @return reporte estadistico
+     * Metodo que genera un reporte estadistico sobre los cubiculos registrados
+     *
+     * @return reporte estadistico
      * @throws excepciones.NegocioException
-   */
+     */
     @Override
-    public List<ReporteResumenCubiculoDTO> generarReporteEstadisticoResumen() throws NegocioException{
+    public List<ReporteResumenCubiculoDTO> generarReporteEstadisticoResumen() throws NegocioException {
         try {
             List<Cita> citas = citaDAO.obtenerCitas();
             List<Cubiculo> cubiculos = cubiculoDAO.buscarCubiculos();
-            
+
             Map<String, Integer> totalCitas = new HashMap<>();
             Map<String, Double> ingresos = new HashMap<>();
-            
+
             for (Cita cita : citas) {
-                String cubiculo = cita.getCubiculo(); 
-                double monto = (cita.getAdeudo() != null && cita.getAdeudo().isEstado())
-                        ? cita.getAdeudo().getCantidad() : 0;
-                
-                totalCitas.put(cubiculo, totalCitas.getOrDefault(cubiculo, 0) + 1);
-                ingresos.put(cubiculo, ingresos.getOrDefault(cubiculo, 0.0) + monto);
+//                String cubiculo = cita.getCubiculo();
+//                double monto = (cita.getAdeudo() != null && cita.getAdeudo().isEstado())
+//                        ? cita.getAdeudo().getCantidad() : 0;
+//
+//                totalCitas.put(cubiculo, totalCitas.getOrDefault(cubiculo, 0) + 1);
+//                ingresos.put(cubiculo, ingresos.getOrDefault(cubiculo, 0.0) + monto);
             }
-            
+
             List<ReporteResumenCubiculoDTO> resumen = new ArrayList<>();
             for (Cubiculo cubiculo : cubiculos) {
                 String nombre = cubiculo.getNombre();
                 int citasCubiculo = totalCitas.getOrDefault(nombre, 0);
                 double ingresoCubiculo = ingresos.getOrDefault(nombre, 0.0);
-                double porcentajeUso = calcularPorcentajeUso(nombre, citasCubiculo); 
+                double porcentajeUso = calcularPorcentajeUso(nombre, citasCubiculo);
                 resumen.add(new ReporteResumenCubiculoDTO(nombre, citasCubiculo, ingresoCubiculo, porcentajeUso));
             }
-            
+
             return resumen;
         } catch (PersistenciaException ex) {
             Logger.getLogger(ReporteCubiculoBO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
-    
+
     private double calcularPorcentajeUso(String nombreCubiculo, int citasCubiculo) {
-        int diasLaborales = 22; 
-        int horasDisponiblesPorDia = 8;
-
-        int totalPosiblesCitas = diasLaborales * horasDisponiblesPorDia;
-
-        if (totalPosiblesCitas == 0) return 0;
-
-        return (citasCubiculo * 100.0) / totalPosiblesCitas;
+//        int diasLaborales = 22;
+//        int horasDisponiblesPorDia = 8;
+//
+//        int totalPosiblesCitas = diasLaborales * horasDisponiblesPorDia;
+//
+//        if (totalPosiblesCitas == 0) {
+//            return 0;
+//        }
+//
+//        return (citasCubiculo * 100.0) / totalPosiblesCitas;
+        return 0.0;
     }
 
     /**
-   * Metodo para obtener un reporte de ingresos de un cubiculo especifico
-   * @param nombreCubiculo Nombre del cubiculo al que se le va a generar el cubiculo
-   * @return Reporte de ingresos de cubiculo
-   * @throws NegocioException 
-   */
+     * Metodo para obtener un reporte de ingresos de un cubiculo especifico
+     *
+     * @param nombreCubiculo Nombre del cubiculo al que se le va a generar el
+     * cubiculo
+     * @return Reporte de ingresos de cubiculo
+     * @throws NegocioException
+     */
     @Override
     public ReporteIngresosCubiculoDTO generarReporteIngresosPorCubiculo(String nombreCubiculo) throws NegocioException {
-        try {
-            List<Cita> citas = citaDAO.obtenerCitas();
-            
-            double ingreso = 0.0;
-            int pagadas = 0;
-            int pendientes = 0;
-            
-            for (Cita cita : citas) {
-                if (nombreCubiculo.equalsIgnoreCase(cita.getCubiculo())) {
-                    Adeudo adeudo = cita.getAdeudo();
-                    if (adeudo != null) {
-                        if (adeudo.isEstado()) { 
-                            ingreso += adeudo.getCantidad();
-                            pagadas++;
-                        } else {
-                            pendientes++;
-                        }
-                    }
-                }
-            }
-            
-            return new ReporteIngresosCubiculoDTO(nombreCubiculo, ingreso, pagadas, pendientes);
-        } catch (PersistenciaException ex) {
-            Logger.getLogger(ReporteCubiculoBO.class.getName()).log(Level.SEVERE, null, ex);
-            throw new NegocioException("Error al generar el reporte", ex);
-        }
+//        try {
+//            List<Cita> citas = citaDAO.obtenerCitas();
+//            
+//            double ingreso = 0.0;
+//            int pagadas = 0;
+//            int pendientes = 0;
+//            
+//            for (Cita cita : citas) {
+//                if (nombreCubiculo.equalsIgnoreCase(cita.getCubiculo())) {
+//                    Adeudo adeudo = cita.getAdeudo();
+//                    if (adeudo != null) {
+//                        if (adeudo.isEstado()) { 
+//                            ingreso += adeudo.getCantidad();
+//                            pagadas++;
+//                        } else {
+//                            pendientes++;
+//                        }
+//                    }
+//                }
+//            }
+//            
+//            return new ReporteIngresosCubiculoDTO(nombreCubiculo, ingreso, pagadas, pendientes);
+//        } catch (PersistenciaException ex) {
+//            Logger.getLogger(ReporteCubiculoBO.class.getName()).log(Level.SEVERE, null, ex);
+//            throw new NegocioException("Error al generar el reporte", ex);
+//        }
+        return null;
     }
-    
+
 }
--- objetos_negocio/src/main/java/mapper/CitaMapper.java
+++ objetos_negocio/src/main/java/mapper/CitaMapper.java
@@ -22,38 +22,43 @@ import java.util.List;
 public class CitaMapper {
 
     public CitaNuevaDTO toDTO(Cita cita) {
-        return new CitaNuevaDTO(cita.getFechaHora(), cita.getCubiculo(), new PsicologoCitaDTO(cita.getPsicologo().getNombre(), cita.getPsicologo().getApellidoPaterno(), cita.getPsicologo().getApellidoMaterno(), cita.getPsicologo().getCorreo(), null),
-                cita.getNombrePaciente(), cita.getTelefonoPaciente(), cita.getCorreoPaciente(), new AdeudoCitaDTO(cita.getAdeudo().getCantidad(), cita.getAdeudo().isEstado()));
+//        return new CitaNuevaDTO(cita.getFechaHora(), cita.getCubiculo(), new PsicologoCitaDTO(cita.getPsicologo().getNombre(), cita.getPsicologo().getApellidoPaterno(), cita.getPsicologo().getApellidoMaterno(), cita.getPsicologo().getCorreo(), null),
+//                cita.getNombrePaciente(), cita.getTelefonoPaciente(), cita.getCorreoPaciente(), new AdeudoCitaDTO(cita.getAdeudo().getCantidad(), cita.getAdeudo().isEstado()));
+        return null;
     }
 
     public Cita toEntity(CitaNuevaDTO cita) {
-        return new Cita(cita.getFechaHora(), cita.getCubiculo(), new Psicologo(null, cita.getPsicologo().getNombre(), cita.getPsicologo().getApellidoPaterno(), cita.getPsicologo().getApellidoMaterno(), cita.getPsicologo().getCorreo(), null),
-                cita.getNombrePaciente(), cita.getTelefonoPaciente(), cita.getCorreoPaciente(), new Adeudo(cita.getAdeudo().getCantidad(), cita.getAdeudo().isEstado()));
+//        return new Cita(cita.getFechaHora(), cita.getCubiculo(), new Psicologo(null, cita.getPsicologo().getNombre(), cita.getPsicologo().getApellidoPaterno(), cita.getPsicologo().getApellidoMaterno(), cita.getPsicologo().getCorreo(), null),
+//                cita.getNombrePaciente(), cita.getTelefonoPaciente(), cita.getCorreoPaciente(), new Adeudo(cita.getAdeudo().getCantidad(), cita.getAdeudo().isEstado()));
+        return null;
     }
 
     public List<CitaNuevaDTO> toDTOList(List<Cita> citas) {
-        List<CitaNuevaDTO> citasDTO = new ArrayList<>();
-        for (Cita cita : citas) {
-            citasDTO.add(new CitaNuevaDTO(cita.getFechaHora(), cita.getCubiculo(), new PsicologoCitaDTO(cita.getPsicologo().getNombre(), cita.getPsicologo().getApellidoPaterno(), cita.getPsicologo().getApellidoMaterno(), cita.getPsicologo().getCorreo(), null),
-                    cita.getNombrePaciente(), cita.getTelefonoPaciente(), cita.getCorreoPaciente(), new AdeudoCitaDTO(cita.getAdeudo().getCantidad(), cita.getAdeudo().isEstado())));
-        }
-        return citasDTO;
+//        List<CitaNuevaDTO> citasDTO = new ArrayList<>();
+//        for (Cita cita : citas) {
+//            citasDTO.add(new CitaNuevaDTO(cita.getFechaHora(), cita.getCubiculo(), new PsicologoCitaDTO(cita.getPsicologo().getNombre(), cita.getPsicologo().getApellidoPaterno(), cita.getPsicologo().getApellidoMaterno(), cita.getPsicologo().getCorreo(), null),
+//                    cita.getNombrePaciente(), cita.getTelefonoPaciente(), cita.getCorreoPaciente(), new AdeudoCitaDTO(cita.getAdeudo().getCantidad(), cita.getAdeudo().isEstado())));
+//        }
+//        return citasDTO;
+        return null;
     }
 
     public List<CitaDTO> toDTOList2(List<Cita> citas) {
-        List<CitaDTO> citasDTO = new ArrayList<>();
-        for (Cita cita : citas) {
-            citasDTO.add(new CitaDTO(cita.getFechaHora(), cita.getCubiculo()));
-        }
-        return citasDTO;
+//        List<CitaDTO> citasDTO = new ArrayList<>();
+//        for (Cita cita : citas) {
+//            citasDTO.add(new CitaDTO(cita.getFechaHora(), cita.getCubiculo()));
+//        }
+//        return citasDTO;
+        return null;
     }
-    
-    public List<CitaRegistradaDTO> toDTOList3 (List<Cita> citas) {
-        List<CitaRegistradaDTO> citasDTO = new ArrayList<>();
-        PsicologoMapper mapper = new PsicologoMapper();
-        for (Cita cita : citas) {
-//            citasDTO.add(new CitaRegistradaDTO(cita.getId(), cita.getFechaHora(), cita.getCubiculo(), mapper.toDTO2(cita.getPsicologo()), cita.getNombrePaciente(), cita.getTelefonoPaciente(), cita.getCorreoPaciente()));
-        }
-        return citasDTO;
+
+    public List<CitaRegistradaDTO> toDTOList3(List<Cita> citas) {
+//        List<CitaRegistradaDTO> citasDTO = new ArrayList<>();
+//        PsicologoMapper mapper = new PsicologoMapper();
+//        for (Cita cita : citas) {
+        ////            citasDTO.add(new CitaRegistradaDTO(cita.getId(), cita.getFechaHora(), cita.getCubiculo(), mapper.toDTO2(cita.getPsicologo()), cita.getNombrePaciente(), cita.getTelefonoPaciente(), cita.getCorreoPaciente()));
+//        }
+//        return citasDTO;
+        return null;
     }
 }
